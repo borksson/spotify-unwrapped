561d2be8a9b68c9f8c7fd19ceea693d54f3c62aa
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-10-16 12:23:53 -0600

    Top Tracks Feature: - added some basic models (Album, Artist, Track) - added a get dummy track function - added a router - added the top tracks page - added the top tracks table item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import TopTracksPage from './pages/TopTracksPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <ChakraProvider>
-      <p>Welcome to Spotify Unwrapped!</p>
+      <BrowserRouter>
+        <Routes>
+          {/* TODO: Add with navbar*/}
+          {/* TODO: Replace with homepage */}
+          <Route path="/" element={<p>Welcome to Spotify Unwrapped!</p>}/>
+          <Route path="/top-tracks" element={<TopTracksPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   )
 }
