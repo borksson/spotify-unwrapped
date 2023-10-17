0843270759202f82b56b146b4777456f42c1d539
drewhartsfield <drewharts8@gmail.com>
2023-10-17 10:38:03 -0600

    - implemented login - implemented auth flow - stored profile and auth token in session storage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TopTracksPage from './pages/TopTracksPage';
 import TopArtistsPage from './pages/TopArtistsPage';
 import WelcomePage from './pages/WelcomePage'; // Import the new component
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Routes>
           {/* TODO: Add with navbar*/}
           {/* TODO: Replace with homepage */}
-          <Route path="/" element={<WelcomePage />} /> {/* Updated this line */}
+          <Route path="/" element={<Login />} /> 
+          <Route path="/home" element={<WelcomePage />} /> {/* Updated this line */}
           <Route path="/top-tracks" element={<TopTracksPage />} />
           <Route path="/top-artists" element={<TopArtistsPage />} />
         </Routes>
