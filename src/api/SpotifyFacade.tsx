f72726cc0102eab9ccc297f9ffaa4ec136c82243
Alex Qian <qianalex8@gmail.com>
2023-10-24 13:42:43 -0600

    added api call for getting top artists

--- src/api/SpotifyFacade.tsx
+++ src/api/SpotifyFacade.tsx
@@ -1,7 +1,5 @@
-// `getTopTracks(accessToken, limit: 5, timeFrame: short_term) -> Tracks[]`
-// Gets a list of users top tracks over a time period. Returns a list of track objects.
-
 import { Track } from "../model/Track";
+import { Artist } from "../model/Artist";
 
 export enum TimeFrame {
     short_term = "short_term",
@@ -9,10 +7,22 @@ export enum TimeFrame {
     long_term = "long_term"
 }
 
+// `getTopTracks(accessToken, limit: 5, timeFrame: short_term) -> Tracks[]`
+// Gets a list of users top tracks over a time period. Returns a list of track objects.
 export const getTopTracks = async (accessToken: string, limit: number, timeFrame: TimeFrame): Promise<Track[]> => {
     const result = await fetch(`https://api.spotify.com/v1/me/top/tracks?limit=${limit}&time_range=${timeFrame}`, {
         method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
     });
     const { items } = await result.json();
     return items as Track[];
+}
+
+// `getTopArtists(accessToken, limit: 5, timeFrame: short_term) -> Artist[]`
+// Gets a list of users top artists over a time period. Returns a list of artist objects.
+export const getTopArtists = async (accessToken: string, limit: number, timeFrame: TimeFrame): Promise<Artist[]> => {
+    const result = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=${limit}&time_range=${timeFrame}`, {
+        method: "GET", headers: { Authorization: `Bearer ${accessToken}` }
+    });
+    const { items } = await result.json();
+    return items as Artist[];
 }
