cded79898ba2df7f9f47ad5f01ac2938311a56fc
Lucy Barton <swimmerlucy963@gmail.com>
2023-10-24 18:51:36 -0600

    add dummy data to user profile page for profile stats

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Image, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { Box, Image, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import { LoginUserProfile } from '../model/LoginUserProfile';
 import { Profile } from '../model/LoginUserProfile';
+import {UserProfile, getDummyUserProfile} from '../model/UserProfile'
 
 export default function UserProfilePage() {
     const [userProfile, setUserProfile] = useState<LoginUserProfile | null>(null);
     const [listeningProfile, setListeningProfile] = useState<Profile | null>(null);
+    const [userProfileStats, setUserProfileStats] = useState<UserProfile | null>(null);
 
     useEffect(() => {
         // TODO: Fetch user profile and listening profile using API calls
         // For now, we'll use dummy data
 
+        setUserProfileStats(
+            getDummyUserProfile()
+        )
         // setUserProfile(dummyUserProfileData);
         // setListeningProfile(dummyListeningProfileData);
     }, []);
@@ -27,7 +32,7 @@ export default function UserProfilePage() {
                 </Box>
             </Box>
 
-            <Box>
+            <TableContainer border={"1px solid LightGray"} borderRadius={"md"} padding={"5px"} margin={"5px"}>
                 <Table variant="simple">
                     <Thead>
                         <Tr>
@@ -36,16 +41,61 @@ export default function UserProfilePage() {
                         </Tr>
                     </Thead>
                     <Tbody>
-                        {/* Map through the listeningProfile and display each attribute */}
-                        {listeningProfile?.map((item, index) => (
-                            <Tr key={index}>
-                                <Td>{Object.keys(item)[0]}</Td>
-                                <Td>{Object.values(item)[0]}</Td>
-                            </Tr>
-                        ))}
+                        <Tr>
+                            <Td>Acousticness</Td>
+                            <Td>{userProfileStats?.acousticness}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Danceability</Td>
+                            <Td>{userProfileStats?.danceability}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Duration (ms)</Td>
+                            <Td>{userProfileStats?.duration_ms}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Energy</Td>
+                            <Td>{userProfileStats?.energy}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Instrumentalness</Td>
+                            <Td>{userProfileStats?.instrumentalness}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Key</Td>
+                            <Td>{userProfileStats?.key}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Liveness</Td>
+                            <Td>{userProfileStats?.liveness}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Loudness</Td>
+                            <Td>{userProfileStats?.loudness}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Mode</Td>
+                            <Td>{userProfileStats?.mode}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Speechiness</Td>
+                            <Td>{userProfileStats?.speechiness}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Tempo</Td>
+                            <Td>{userProfileStats?.tempo}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Time Signature</Td>
+                            <Td>{userProfileStats?.time_signature}</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>Valence</Td>
+                            <Td>{userProfileStats?.valence}</Td>
+                        </Tr>
                     </Tbody>
                 </Table>
-            </Box>
+            </TableContainer>
         </Box>
     );
 }
