e35364299c1219579b10a26000c0815ef690b69b
Lucy Barton <swimmerlucy963@gmail.com>
2023-11-09 09:56:44 -0700

    make items on tables clickable

--- src/components/ArtistTableItem.tsx
+++ src/components/ArtistTableItem.tsx
@@ -11,9 +11,13 @@ export default function ArtistTableItem({ artist, rank }: ArtistTableItemProps) 
         <Tr key={artist.id} _hover={{ bg: "green.100", color: "black" }}>
             <Th color="gray.150">{rank}</Th>
             <Th color="gray.150">
-            <ChakraImage boxSize="50px" src={artist.images?.[0]?.url} alt={artist.name} mr={2} />
+                <a href={artist.uri}>
+                    <ChakraImage boxSize="50px" src={artist.images?.[0]?.url} alt={artist.name} mr={2} />
+                </a>
             </Th>
-            <Th color="gray.150">{artist.name}</Th>
+            <Th color="gray.150">
+                <a href={artist.uri}>{artist.name}</a>
+            </Th>
         </Tr>
     )
 }
