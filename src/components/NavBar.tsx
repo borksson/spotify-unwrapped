12e50e3eab2e485258ae9d5ddb3c0ddc930ab35e
DallinVetter <89808431+DallinVetter@users.noreply.github.com>
2023-11-14 17:02:07 -0700

    Implemented pages into nav-bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,36 +16,37 @@ import {
   useDisclosure,
   useColorModeValue,
   Stack,
-} from '@chakra-ui/react'
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { Link } from 'react-router-dom';
 
-interface Props {
-  children: React.ReactNode
+interface NavLinkProps {
+  children: React.ReactNode;
+  to: string;
 }
 
-const Links = ['Dashboard', 'Projects', 'Team']
+const NavLink = ({ children, to }: NavLinkProps) => (
+  <Box
+    as={Link}
+    to={to}
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{ textDecoration: 'none', bg: useColorModeValue('gray.200', 'gray.700') }}
+  >
+    {children}
+  </Box>
+);
 
-const NavLink = (props: Props) => {
-  const { children } = props
+const Links = [
+  { name: 'Home', to: '/home' },
+  { name: 'Top Tracks', to: '/top-tracks' },
+  { name: 'Top Artists', to: '/top-artists' },
+  { name: 'User Profile', to: '/user-profile' }
+];
 
-  return (
-    <Box
-      as="a"
-      px={2}
-      py={1}
-      rounded={'md'}
-      _hover={{
-        textDecoration: 'none',
-        bg: useColorModeValue('gray.200', 'gray.700'),
-      }}
-      href={'#'}>
-      {children}
-    </Box>
-  )
-}
-
-export default function Simple() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+export default function NavBar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
@@ -59,10 +60,13 @@ export default function Simple() {
             onClick={isOpen ? onClose : onOpen}
           />
           <HStack spacing={8} alignItems={'center'}>
-            <Box>Logo</Box>
-            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+            <Box>Spotify Unwrapped</Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink key={link.name} to={link.to}>{link.name}</NavLink>
               ))}
             </HStack>
           </HStack>
@@ -77,15 +81,15 @@ export default function Simple() {
                 <Avatar
                   size={'sm'}
                   src={
-                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    'https://avatars.dicebear.com/api/male/username.svg'
                   }
                 />
               </MenuButton>
               <MenuList>
-                <MenuItem>Link 1</MenuItem>
-                <MenuItem>Link 2</MenuItem>
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>Settings</MenuItem>
                 <MenuDivider />
-                <MenuItem>Link 3</MenuItem>
+                <MenuItem>Sign out</MenuItem>
               </MenuList>
             </Menu>
           </Flex>
@@ -95,12 +99,12 @@ export default function Simple() {
           <Box pb={4} display={{ md: 'none' }}>
             <Stack as={'nav'} spacing={4}>
               {Links.map((link) => (
-                <NavLink key={link}>{link}</NavLink>
+                <NavLink key={link.name} to={link.to}>{link.name}</NavLink>
               ))}
             </Stack>
           </Box>
         ) : null}
       </Box>
     </>
-  )
+  );
 }
